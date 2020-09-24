106bf04a91a36bc2f51655c555223af3ab073beb
Artem <artem.baturov@bk.ru>
2020-09-24 03:07:37 +0300

    add form & slider

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/slider/chevron_left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/slider/chevron_right.png"></button>'
+    });
+});
